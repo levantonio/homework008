dfc37cb61698e51e18ca513284a2c4ac2bdce911
Anton <levantonio@mail.ru>
2022-12-07 15:28:22 +0300

    task 60

---
+++ task60/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из 
+// неповторяющихся двузначных чисел. Напишите программу, 
+// которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Например, задан массив размером 2 x 2 x 2.
+// Результат:
+
+// 66(0,0,0) 27(0,0,1) 25(0,1,0) 90(0,1,1)
+// 34(1,0,0) 26(1,0,1) 41(1,1,0) 55(1,1,1)
+
+int[,,] CreateCubeRndIntMatrix(int rows, int columns, int depth, int min, int max)
+{
+    int[,,] cubematrix = new int[rows, columns, depth]; //строка и колонка
+
+    Random rnd = new Random(); //надо делать так
+    for (int i = 0; i < cubematrix.GetLength(0); i++)
+    {   
+        for (int j = 0; j < cubematrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < cubematrix.GetLength(2); k++)
+            {
+                cubematrix[i, j, k] = rnd.Next(min, max + 1); //i = индекс массива
+
+                if(i == j && j == k) 
+                {
+                    cubematrix[i, j, k] = rnd.Next(min, max + 1); //i = индекс массива
+
+                }
+            }
+        }
+    }
+    return cubematrix;
+}
+
+void PrintCubeMatrix(int[,,] cubematrix)
+{
+    for (int i = 0; i < cubematrix.GetLength(0); i++)
+    {
+        Console.Write("[     ");
+        for (int j = 0; j < cubematrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < cubematrix.GetLength(2); k++)
+            {
+                Console.Write(cubematrix[i, j, k]);
+                Console.Write("({0},{1},{2})\t", i, j, k);
+            }
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[,,] array3D = CreateCubeRndIntMatrix(2, 2, 2, 10, 99);
+PrintCubeMatrix(array3D);
